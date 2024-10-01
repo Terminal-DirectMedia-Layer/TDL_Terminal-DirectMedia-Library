d5909a71e01405d96a4c15913735d91baf8cbb21
Shin-gema <thomas.janiaut@epitech.eu>
2024-10-01 13:14:53 +0200

    fix(event): unused gpm library
    The use of Gpm was a old mouse detection method that is deprecated in TDL

--- source/TDL/Event/Mouse/Linux/Mouse.cpp
+++ source/TDL/Event/Mouse/Linux/Mouse.cpp
@@ -3,7 +3,6 @@
 
 #include <fcntl.h>
 #include <string.h>
-#include <gpm.h>
 
 #include "TDL/Event/Mouse/Linux/Mouse.hpp"
 #include "TDL/Event/Mouse/EventMouseData.hpp"
@@ -15,7 +14,7 @@ bool tdl::Mouse::mouseMove(AWindow *window, EventMouseData data)
 {
     if (data._button == 32 || data._button == 35) {
         Event event;
-        event.type = Event::EventType::MOUSEMOVED;
+        event.type = TDL_MOUSEMOVED;
         event.mouseMove.x = data._x * 2;
         event.mouseMove.y = data._y * 3;
         window->pushEvent(event);
@@ -31,22 +30,22 @@ bool tdl::Mouse::mousePessed(AWindow *window, EventMouseData data)
         return false;
     }
     if (data._button == 0) {
-        event.type = Event::EventType::MOUSEBUTTONPRESSED;
-        event.mouseButton.button = MouseButton::LEFT;
+        event.type = TDL_MOUSEPRESSED;
+        event.mouseButton.button = TDL_MOUSELEFT;
         event.mouseButton.x = data._x * 2;
         event.mouseButton.y = data._y * 3;
         window->pushEvent(event);
         return true;
     } else if (data._button == 1) {
-        event.type = Event::EventType::MOUSEBUTTONPRESSED;
-        event.mouseButton.button = MouseButton::MIDDLE;
+        event.type = TDL_MOUSEPRESSED;
+        event.mouseButton.button = TDL_MOUSEMIDDLE;
         event.mouseButton.x = data._x * 2;
         event.mouseButton.y = data._y * 3;
         window->pushEvent(event);
         return true;
     } else if (data._button == 2) {
-        event.type = Event::EventType::MOUSEBUTTONPRESSED;
-        event.mouseButton.button = MouseButton::RIGHT;
+        event.type = TDL_MOUSEPRESSED;
+        event.mouseButton.button = TDL_MOUSERIGHT;
         event.mouseButton.x = data._x * 2;
         event.mouseButton.y = data._y * 3;
         window->pushEvent(event);
@@ -62,22 +61,22 @@ bool tdl::Mouse::mouseReleased(AWindow *window, EventMouseData data)
         return false;
     }
     if (data._button == 0) {
-        event.type = Event::EventType::MOUSEBUTTONRELEASED;
-        event.mouseButton.button = MouseButton::LEFT;
+        event.type = TDL_MOUSERELEASED;
+        event.mouseButton.button = TDL_MOUSELEFT;
         event.mouseButton.x = data._x * 2;
         event.mouseButton.y = data._y * 3;
         window->pushEvent(event);
         return true;
     } else if (data._button == 1) {
-        event.type = Event::EventType::MOUSEBUTTONRELEASED;
-        event.mouseButton.button = MouseButton::MIDDLE;
+        event.type = TDL_MOUSERELEASED;
+        event.mouseButton.button = TDL_MOUSEMIDDLE;
         event.mouseButton.x = data._x * 2;
         event.mouseButton.y = data._y * 3;
         window->pushEvent(event);
         return true;
     } else if (data._button == 2) {
-        event.type = Event::EventType::MOUSEBUTTONRELEASED;
-        event.mouseButton.button = MouseButton::RIGHT;
+        event.type = TDL_MOUSERELEASED;
+        event.mouseButton.button = TDL_MOUSERIGHT;
         event.mouseButton.x = data._x * 2;
         event.mouseButton.y = data._y * 3;
         window->pushEvent(event);
@@ -90,15 +89,15 @@ bool tdl::Mouse::mouseScroll(AWindow *window, EventMouseData data)
 {
     Event event;
     if (data._button == 64) {
-        event.type = Event::EventType::MOUSESCROLLED;
-        event.mouseScroll.direction = MouseButton::UP;
+        event.type = TDL_MOUSEWHEEL;
+        event.mouseScroll.direction = TDL_WHEELUP;
         event.mouseScroll.x = data._x * 2;
         event.mouseScroll.y = data._y * 3;
         window->pushEvent(event);
         return true;
     } else if (data._button == 65) {
-        event.type = Event::EventType::MOUSESCROLLED;
-        event.mouseScroll.direction = MouseButton::DOWN;
+        event.type = TDL_MOUSEWHEEL;
+        event.mouseScroll.direction = TDL_WHEELUP;
         event.mouseScroll.x = data._x * 2;
         event.mouseScroll.y = data._y * 3;
         window->pushEvent(event);
